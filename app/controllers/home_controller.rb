9ce6c7587bac4bbd853b72ea79fa529156bbf6ef
Fercell <galdiku.andrius@gmail.com>
2017-07-04 08:27:44 +0300

    database.yml & font-awesome

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,11 @@
 class HomeController < ApplicationController
 
 	def index
-		@owner = User.where(isOwner: true).limit(1).first		
+		@owner = User.where(isOwner: true).limit(1).first
 		@experiences = Experience.where(id: @owner.id)
 		# @educations = Education.where(id: @owner.id)
 		@educations = Education.all
-		@workshops = Workshop.where(id: @owner.id) 
+		@workshops = Workshop.where(id: @owner.id)
 		@languages = Language.where(id: @owner.id)
 		@skills = Skill.where(id: @owner.id)
 		@interests = Interest.where(id: @owner.id)
