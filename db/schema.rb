84e421f0967eb74cb8a7c614b25110b1993ac675
Viktorija <s.vikvik@gmail.com>
2017-10-30 19:02:30 +0200

    Dates added for educations and new translations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171030164254) do
+ActiveRecord::Schema.define(version: 20171030165836) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -386,8 +386,6 @@ ActiveRecord::Schema.define(version: 20171030164254) do
     t.inet "last_sign_in_ip"
     t.boolean "isOwner"
     t.boolean "admin", default: false
-    t.string "name"
-    t.index "lower((name)::text) text_pattern_ops", name: "users_name_lower", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
