2db421e90801d1a1690cce5d474bdccf674086b8
Viktorija <s.vikvik@gmail.com>
2017-11-23 13:21:45 +0200

    permit additional params for signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
 
 	def after_sign_out_path_for(resource)
@@ -18,6 +19,11 @@ class ApplicationController < ActionController::Base
 	def default_url_options(options = {})
 		{locale: I18n.locale}
 	end
-
+	
+	def configure_permitted_parameters
+	  devise_parameter_sanitizer.permit(:sign_up) do |user_params|
+	    user_params.permit(:username, :email, :password, :password_confirmation)
+	  end
+	end
 
 end
